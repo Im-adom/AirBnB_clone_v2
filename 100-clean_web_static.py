5b5af349e970745f639412682fc976fa38c89069
Im-adom <graceamaningkwarteng@gmail.com>
2023-11-09 11:55:43 -0800

    web static

---
+++ 100-clean_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" This function deletes out of date achives """
+from fabric.api import *
+
+
+env.hosts = ['54.162.238.148', '52.90.14.171']
+env.user = "ubuntu"
+
+
+def do_clean(number=0):
+    """ CLEANS """
+
+    number = int(number)
+
+    if number == 0:
+        number = 2
+    else:
+        number += 1
+
+    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    path = '/data/web_static/releases'
+    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
